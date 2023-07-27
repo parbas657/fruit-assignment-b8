78251ad759f9c729fc31e2a09ac470646fc99381
SK ALMAS <mparbas657@gmail.com>
2023-07-27 23:35:30 +0600

    complect project

--- images/tailwind.config.js
+++ images/tailwind.config.js
@@ -6,3 +6,30 @@ module.exports = {
     },
     plugins: [],
   }
+
+
+
+
+//   <!-- <header>
+//   <div class="hero min-h-screen" style="background-image: url(../images/banner2.png);">
+//     <div class="hero-overlay bg-opacity-60"></div>
+//     <div class="hero-content text-center text-neutral-content">
+//       <div class="max-w-md">
+//         <h1 class="mb-5 text-5xl font-bold">Exotic tastes at your <br> dorstep</h1>
+//         <p class="mb-5">we supply highly quality orange products</p>
+//         <button class="btn btn-primary">Shop Now</button>
+//       </div>
+//     </div>
+//   </div>
+// </header> -->
+
+
+
+
+
+
+
+
+
+
+
