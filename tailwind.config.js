d7be11cf03c5dc6ce42c8f12d04ad78a60a9db7e
SK ALMAS <mparbas657@gmail.com>
2023-08-02 16:20:31 +0600

    work start

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+
+
+//   <!-- <header>
+//   <div class="hero min-h-screen" style="background-image: url(../images/banner2.png);">
+//     <div class="hero-overlay bg-opacity-60"></div>
+//     <div class="hero-content text-center text-neutral-content">
+//       <div class="max-w-md">
+//         <h1 class="mb-5 text-5xl font-bold">Exotic tastes at your <br> dorstep</h1>
+//         <p class="mb-5">we supply highly quality orange products</p>
+//         <button class="btn btn-primary">Shop Now</button>
+//       </div>
+//     </div>
+//   </div>
+// </header> -->
+
+
+
+
+
+
+
+
+
+
+
